f6234079e7e5bb46d84fa970aa914021e8f3a210
dw3624 <dw3624@gmail.com>
2020-06-11 08:11:08 +0900

    shiny

--- shiny/server.R
+++ shiny/server.R
@@ -9,7 +9,7 @@ shinyServer(function(input, output){
   #Functions/Variables for each session
   
   #Import Files
-  source('server_est3.R',local=TRUE)
+  source('server_est.R',local=TRUE)
   source('server_est2.R',local=TRUE)
   
   
