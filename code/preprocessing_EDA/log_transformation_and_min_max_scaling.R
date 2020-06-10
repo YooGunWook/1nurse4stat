313b41b02b0c8bf38e8fb18e3f36d4fb1ff39acf
dhxoghks95 <dhxoghks95@gmail.com>
2020-06-10 16:12:36 +0900

    title_song 추가 데이터 log transformation & minmax scaling

--- code/preprocessing_EDA/log_transformation_and_min_max_scaling.R
+++ code/preprocessing_EDA/log_transformation_and_min_max_scaling.R
@@ -1,44 +1,20 @@
 library(tidyverse)
 library(tidymodels)
-data = read.csv("C:/Users/dhxog/dsintro/1nurse4stat/data/model_data/파생변수_최종.csv")
-
+data = read_csv("C:/Users/dhxog/dsintro/1nurse4stat/data/model_data/NA_제거.csv")
+summary(data)
+data = data[,-1]
 summary(data)
 skimr::skim(data)
 
-
-data = data %>% select(artist, name, rank_g, 순위, previous_ranking, genre, season, sex, song_type, active_type, runtime, top_freq, gg_score, nv_score, 조회수, season_genre_score, pd_score, dc_total_numb, dc_mean_reccomend, dc_mean_views, view, title_song, 주차)
-
-
-data = rename(data,  "you_rank" = "순위")
-data = rename(data, "drama_view" = "view")
-data = rename(data, "total_view" = "조회수")
-data = rename(data, "week" = "주차")
-
-data = data %>% mutate(you_rank_g=case_when(1<=you_rank&you_rank<=10~1,
-                                           11<=you_rank&you_rank<=20~2,
-                                           21<=you_rank&you_rank<=30~3,
-                                           31<=you_rank&you_rank<=40~4,
-                                           41<=you_rank&you_rank<=50~5,
-                                           51<=you_rank&you_rank<=60~6,
-                                           61<=you_rank&you_rank<=70~7,
-                                           71<=you_rank&you_rank<=80~8,
-                                           81<=you_rank&you_rank<=90~9,
-                                           TRUE~10))
-
-
-
-data = data %>% select(-you_rank)
-
-write.csv(data, "NA_채우기_전.csv")
 ## Skwed data log transformation
 
 ### 1) nv_score
 data%>% select(nv_score) %>% ggplot(aes(x = nv_score)) + geom_density()
 
-data %>% select(nv_score) %>% transmute(nv_score = log(nv_score)) %>%ggplot(aes(x = nv_score)) + geom_density()
+data %>% select(nv_score) %>% transmute(nv_score = log(nv_score)) %>%ggplot(aes(x = nv_score + 1)) + geom_density()
 
 
-data$nv_score = data %>% select(nv_score) %>% transmute(nv_score = log(nv_score))
+data$nv_score = data %>% select(nv_score) %>% transmute(nv_score = log(nv_score + 1))
 
 ## 2) total_view drama_view
 
@@ -103,7 +79,7 @@ normalize <- function(x)
 
 numeric_data = data %>% select(runtime, top_freq, gg_score, nv_score, total_view, season_genre_score, pd_score, dc_total_numb, dc_mean_reccomend, dc_mean_views, drama_view)
 
-cat_data = data %>% select(rank_g, you_rank_g, previous_ranking, genre, season, sex, song_type, active_type)
+cat_data = data %>% select(rank_g, you_rank_g, previous_ranking, genre, season, sex, song_type, active_type, title_song)
 
 for(i in 1:(ncol(numeric_data))){
   numeric_data[,i] = normalize(numeric_data[,i])
@@ -114,11 +90,7 @@ scaled_data = data.frame(cat_data, numeric_data)
 summary(scaled_data)
 
 
-write_csv(scaled_data, "log_scaled_data.csv")
-
-install.packages("data.table", 
-                 repos = "https://Rdatatable.github.io/data.table", type = "source")
 
 library(data.table)
 
-fwrite(scaled_data, file = "log_scaled_data.csv")
+fwrite(scaled_data, file = "log_scaled_data_final.csv")
