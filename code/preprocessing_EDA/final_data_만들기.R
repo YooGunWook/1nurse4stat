efa3688ee4ca31a10ee21f6fddfd902d3a533c92
dhxoghks95 <dhxoghks95@gmail.com>
2020-06-15 15:30:24 +0900

    NA 안없엔거

--- code/preprocessing_EDA/final_data_만들기.R
+++ code/preprocessing_EDA/final_data_만들기.R
@@ -1,15 +1,11 @@
 library(tidyverse)
 
 
-data = read_csv("https://raw.githubusercontent.com/YooGunWook/1nurse4stat/master/data/model_data/%EC%B5%9C%EC%A2%85%20%EA%B2%B0%EA%B3%BC_log_Scale_%EA%B7%B8%EB%9E%98%ED%94%84.csv")
-data = data %>% select(-X1)
+data = read.csv("final_data.csv")
+
+View(data)
 
 
-
-name = read.csv("C:/Users/dhxog/dsintro/1nurse4stat/data/model_data/파생변수_최종.csv")
-
-
-data = data.frame(data, name = name %>% select(name))
 
 library(lubridate)
 data$week = ymd(data$week)
@@ -27,8 +23,7 @@ data = data %>%
   mutate(previous_ranking = ifelse(week == min(week), previous_top, last_week_rank)) %>% ungroup()
 
 data = data %>% select(-c(previous_top, last_week_rank))
-data = data %>% select(-rank_g_pred)
+data = data %>% filter(week > min(week))
 
 
-data$previous_ranking[is.na(data$previous_ranking) == T] = 10
 write.csv(data, "final_data.csv", row.names = FALSE)
